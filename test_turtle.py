02834526dbedb7b0ba92d970aaa5bef5f40011f9
Malek <malek.yousefi.66@gmail.com>
2021-12-17 01:49:57 +0330

    how to work with Turtle

---
+++ test_turtle.py
@@ -0,0 +1,16 @@
+import turtle
+for i in range(5):
+    turtle.forward(100)
+    turtle.left(72)
+# turtle.done()
+# =============================
+from turtle import *
+for i in range (5):
+    forward(200)
+    right(144)
+# done()
+# =============================
+for i in range (360):
+    forward(1)
+    left(1)
+done()
